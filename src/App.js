1b3e50ebdf80a97709095aca2d769469939e0a64
mrbananaaa <personal.astagiriii@gmail.com>
2022-09-22 14:51:57 +0700

    coba ganti

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: 'x-large'
+      }}
+    >
+      Hello Github from React
     </div>
   );
 }
